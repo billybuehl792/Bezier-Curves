1072897bfda02d3cffca859e2a39ffc1ff847ad7
Billy Buehl <billybuehl792@gmail.com>
2021-07-26 10:31:39 -0400

    updated coordinate func

--- bezier.py
+++ bezier.py
@@ -1,15 +1,24 @@
 #!python3
 # bezier_fun.py - bezier plotting
 
-import pandas as pd
 from matplotlib import pyplot as plt
 
 def get_coord(t, t_start, t_stop, start_val, stop_val, ease_out, ease_in):
     # single parameter bezier
     # time on x axis
+    t_prog = 1 / (t_stop - t_start) * (t - t_start)
     p1 = (t_start, start_val)
     p4 = (t_stop, stop_val)
-    p2 = p1[0] + (p4[0] - p1[0]) * 
+    p2 = ((p1[0] + (p4[0] - p1[0]) * ease_out), start_val)
+    p3 = ((p1[0] + (p4[0] - p1[0]) * (1 - ease_in)), stop_val)
+    
+    x = ((1-t_prog)**3 * p1[0]) + (3 * (1-t_prog)**2 * t_prog * p2[0]) + (3 * (1-t_prog) * t_prog**2 * p3[0]) + (t_prog**3 * p4[0])
+    y = ((1-t_prog)**3 * p1[1]) + (3 * (1-t_prog)**2 * t_prog * p2[1]) + (3 * (1-t_prog) * t_prog**2 * p3[1]) + (t_prog**3 * p4[1])
+    
+    print(f'x: {x}, y: {y}, t_prog: {t_prog}, p1: {p1}, p2: {p2}, p3: {p3}, p4: {p4}')
+
+    return (x,y)
+
 def get_coord2(t, p1, p2):
     x = ((1-t) * p1[0]) + (t * p2[0])
     y = ((1-t) * p1[1]) + (t * p2[1])
@@ -33,9 +42,11 @@ if __name__ == '__main__':
     y_positions = []
 
     # animation starts at 2s and goes to 7s
-    times = [2, 3, 4, 5, 6, 7]
+    times = [2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7]
     for t in times:
-        x, y = easeCustom(t, 2, 7, (0,0), (10,10), .33, 1)
+        x, y = get_coord(t, 2, 7, 0, 10, .4, 1)
+        x_positions.append(x)
+        y_positions.append(y)
 
     # for i in range(positions+1):
     #     t = i / positions
@@ -46,7 +57,7 @@ if __name__ == '__main__':
     #     y_positions.append(y)
 
     plt.plot(x_positions, y_positions)
-    plt.title("Bezier Curve")
+    plt.title("stupid curve")
     plt.xlabel = "Time"
     plt.ylabel = "Parameter"
     plt.show()
